60ee30f64d393f17ff273cd7c888eba7c4258266
Raul Vagmacker <raulvagmacker17@gmail.com>
2020-07-15 23:06:21 -0300

    Utilizando Styled Components e estilizando o dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,61 @@
-import React from 'react'
+import React from 'react';
+
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/50815749?s=460&u=956ffe3f410d2ee440edc2f85e8ba4c8b255d547&v=4"
+            alt="Raul Vagmacker"
+          />
+          <div>
+            <strong>iVagmacker/react-typescript-structure</strong>
+            <p>ReactJs, Typescript, Structure and Standards</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/50815749?s=460&u=956ffe3f410d2ee440edc2f85e8ba4c8b255d547&v=4"
+            alt="Raul Vagmacker"
+          />
+          <div>
+            <strong>iVagmacker/react-typescript-structure</strong>
+            <p>ReactJs, Typescript, Structure and Standards</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/50815749?s=460&u=956ffe3f410d2ee440edc2f85e8ba4c8b255d547&v=4"
+            alt="Raul Vagmacker"
+          />
+          <div>
+            <strong>iVagmacker/react-typescript-structure</strong>
+            <p>ReactJs, Typescript, Structure and Standards</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
