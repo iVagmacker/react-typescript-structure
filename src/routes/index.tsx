acb08386d167ee5a0c51f8341872dd2d4b4d4894
Raul Vagmacker <raulvagmacker17@gmail.com>
2020-07-20 18:04:48 -0300

    Navegando entre as rotas, estilizando detalhe e listando issues da API

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,9 +7,8 @@ import Repository from '../pages/Repository';
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Dashborad} />
-    <Route path="/repository" component={Repository} />
-
+    <Route path="/repositories/:repository+" component={Repository} />
   </Switch>
 );
 
-export default Routes
+export default Routes;
